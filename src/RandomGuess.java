1987a18dfc4b300c4b57003761fc11988f10bca3
DenisOlaleye <denis.olaleye@gmail.com>
2022-08-03 13:35:46 +0300

    1

---
+++ src/RandomGuess.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+
+public class RandomGuess 
+{
+	public static void main (String [] args)
+	{
+				
+		JOptionPane.showMessageDialog(null, "Enter number from 1 to 10");		
+		
+		JOptionPane.showMessageDialog(null, "The number is " 
+				+(1+(int)(Math.random()*10)));
+		
+		
+	}
+}
